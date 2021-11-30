af6731938aa7966e24c05deef4fc686cfef19a67
Madadehl <madadehl94@gmail.com>
2021-11-29 22:30:37 -0700

    pruebatoken

---
+++ src/perímetro/figuras.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perímetro;
+
+/**
+ *
+ * @author madad
+ */
+public class figuras 
+{
+    float lado;
+    float base;
+    floataltura;
+    float radio;
+    float perimetro;
+    float pi;
+    
+    void perimetro_circulo()
+            perimetro = 2 * pi * radio
+    
+    void perimetro_cuadrado()
+            
+}
